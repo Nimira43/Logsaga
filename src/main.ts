380b3e923d848e55929b1907072c1bf0b1a42c77
Nimira43 <n.rabone72@aol.com>
2025-12-18 20:52:35 +0000

    providers - 2

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { AppComponent } from './app/app.component'
 import { LucideAngularModule, Server, Activity, Circle, CheckCircle2, AlertTriangle} from 'lucide-angular'
+import { importProvidersFrom } from '@angular/core'
 
 bootstrapApplication(AppComponent, {
   providers: [
-    
+    importProvidersFrom(
+      LucideAngularModule.pick({
+        Server,
+        Activity,
+        Circle,
+        CheckCircle2,
+        AlertTriangle,
+      })
+    )
   ]
 })
   .catch((err) => console.error(err))
