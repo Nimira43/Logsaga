664c6f7437280f1bd97e9f4169b4751d98f9e5bc
Nimira43 <n.rabone72@aol.com>
2025-12-18 20:50:27 +0000

    Lucide Angular imports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { AppComponent } from './app/app.component'
+import { LucideAngularModule, Server, Activity, Circle, CheckCircle2, AlertTriangle} from 'lucide-angular'
 
 bootstrapApplication(AppComponent)
   .catch((err) => console.error(err))
