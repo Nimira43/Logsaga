45e324b5a4982d9ce626c92a043dd6e5263496e2
Nimira43 <n.rabone72@aol.com>
2025-12-19 03:20:45 +0000

    Brought in Lucide icon into HTML. Working fine.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
+import { LucideAngularModule } from 'lucide-angular'
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
