003176f8710725def76a1204486e17cc82e0a3bc
Nimira43 <n.rabone72@aol.com>
2025-12-19 14:34:33 +0000

    Importing LucideAngularModule into server-status html

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
+import { LucideAngularModule } from 'lucide-angular'
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
