b911f56ddf027cb1039f4e324f0e1360cfc20add
Nimira43 <n.rabone72@aol.com>
2025-12-19 14:53:06 +0000

    Moved dummyTrafficData and maxTraffic into traffic ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,43 +13,4 @@ import { ServerStatusComponent } from './dashboard/server-status/server-status.c
   ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  dummyTrafficData = [
-    {
-        id: 'd1',
-        value: 358,
-    },
-    {
-        id: 'd2',
-        value: 473,
-    },
-    {
-        id: 'd3',
-        value: 298,
-    },
-    {
-        id: 'd4',
-        value: 332,
-    },
-    {
-        id: 'd5',
-        value: 490,
-    },
-    {
-        id: 'd6',
-        value: 407,
-    },
-    {
-        id: 'd7',
-        value: 442,
-    },
-  ]
-  maxTraffic = Math
-    .max(
-      ...this.dummyTrafficData
-        .map(
-          (data) => data.value
-        )
-    )
-  
-}
+export class AppComponent {}
--- src/app/dashboard/traffic/traffic.component.ts
+++ src/app/dashboard/traffic/traffic.component.ts
@@ -9,5 +9,39 @@ import { LucideAngularModule } from 'lucide-angular'
   styleUrl: './traffic.component.css'
 })
 export class TrafficComponent {
-
+  dummyTrafficData = [
+    {
+        id: 'd1',
+        value: 358,
+    },
+    {
+        id: 'd2',
+        value: 473,
+    },
+    {
+        id: 'd3',
+        value: 298,
+    },
+    {
+        id: 'd4',
+        value: 332,
+    },
+    {
+        id: 'd5',
+        value: 490,
+    },
+    {
+        id: 'd6',
+        value: 407,
+    },
+    {
+        id: 'd7',
+        value: 442,
+    },
+  ]
+  maxTraffic = Math.max(
+    ...this.dummyTrafficData.map(
+      (data) => data.value
+    )
+  )
 }
