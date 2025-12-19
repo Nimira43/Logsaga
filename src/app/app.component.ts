5ec92d381bf7fce23c466709cd24dac2d54ee8c8
Nimira43 <n.rabone72@aol.com>
2025-12-19 14:39:33 +0000

    Bringing app-server-status into app html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core'
 import { LucideAngularModule } from 'lucide-angular'
 import { HeaderComponent } from './header/header.component'
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LucideAngularModule, HeaderComponent],
+  imports: [
+    LucideAngularModule,
+    HeaderComponent,
+    ServerStatusComponent
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
