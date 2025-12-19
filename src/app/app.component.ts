2c2fc8a730444fd07406028004de5344702d4325
Nimira43 <n.rabone72@aol.com>
2025-12-19 03:37:19 +0000

    dummyTrafficData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,34 @@ import { LucideAngularModule } from 'lucide-angular'
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'logsaga'
+  dummyTrafficData = [
+    {
+        id: 'd1',
+        value: 358,
+    },
+    {
+        id: 'd2',
+        value: 473,
+    },
+    {
+        id: 'd3',
+        value: 298,
+    },
+    {
+        id: 'd4',
+        value: 332,
+    },
+    {
+        id: 'd5',
+        value: 490,
+    },
+    {
+        id: 'd6',
+        value: 407,
+    },
+    {
+        id: 'd7',
+        value: 442,
+    },
+  ]
 }
